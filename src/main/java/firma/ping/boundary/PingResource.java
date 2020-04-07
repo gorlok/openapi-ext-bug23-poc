0ec64ddbfa1dcdbbd64bfdad048045a4ed782264
Phillip Kruger <phillip.kruger@gmail.com>
2020-04-07 10:30:30 +0200

    Fixes Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/firma/ping/boundary/PingResource.java
+++ src/main/java/firma/ping/boundary/PingResource.java
@@ -1,14 +1,28 @@
 package firma.ping.boundary;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("ping")
+@Produces(value = MediaType.TEXT_PLAIN)
 public class PingResource {
 
+    @Inject
+    @ConfigProperty(name = "message")
+    private String message;
+    
     @GET
     public String ping() {
         return "pong";
     }
-
+    
+    @GET
+    @Path("/message")
+    public String message() {
+        return message;
+    }
 }
